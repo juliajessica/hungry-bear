29180171240fd7413c1c242fb07f62673d53d0dd
julia sheremet <juliajessicasher@gmail.com>
2018-04-24 04:43:05 -0700

    work out kinks in program

--- src/hungrybear.js
+++ src/hungrybear.js
@@ -1,25 +1,60 @@
-// export class HungryBear {
-//
-//   constructor(name) {
-//     this.name = name;
-//     this.foodLevel = 10;
-//   }
-//
-//   setHunger() {
-//     setInterval(() => { //arrow function: (callback) let this = that;
-//       this.foodLevel--; //decrementing food level
-//     }, 1000);
-//   }
-//
-//   didYouGetEaten() {
-//     if (this.foodLevel > 0) {
-//       return false;
-//     } else {
-//       return true;
-//     }
-//   }
-//
-//   feed() {
-//     this.foodLevel = 10;
-//   }
+export let bear = { //bear variable: One advantage of this is that these prototypes are all tightly encapsulated within this single variable now. That wouldn’t be so helpful if we needed to make many bears in our application, but there’s only one, so this works just as well as using classes.
+  foodLevel: 10,
+  setHunger: function() {
+    const hungerInterval = setInterval(() => { //arrow notation
+      this.foodLevel--; //decrementing
+      if (this.didYouGetEaten() === true) {
+        clearInterval(hungerInterval);
+        return "You got eaten!";
+      }
+    }, 1000);
+  },
+  didYouGetEaten: function() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  feed: function(amount) {
+    setInterval(() => {
+      that.foodLevel += amount;
+      if (this.foodLevel === true) {
+        return 'The bear ate the ${food}! Food level goes up ${amount}!'
+      }
+    }, 1000);
+  },
+  //  = bear.feed(5);// creating a new property on the bear called eatSmall
+  eatSmall: function(food) {
+    this.foodLevel += 5
+    return 'The bear ate the ${food}! Food level goes up 5!'
+  }
+}; // end of class
+
+
+
+// export class HungryBear { //bear class
+
+  // constructor(name) {
+  //   this.name = name;
+  //   this.foodLevel = 10;
+  // }
+  //
+  // setHunger() {
+  //   setInterval(() => { //arrow function: (callback) let this = that;
+  //     this.foodLevel--; //decrementing food level
+  //   }, 1000);
+  // }
+  //
+  // didYouGetEaten() {
+  //   if (this.foodLevel > 0) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+  //
+  // feed() {
+  //   this.foodLevel = 10;
+  // }
 // } // end of class
--- src/main.js
+++ src/main.js
@@ -1,35 +1,8 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
-//
-// $(document).ready(function(){
-//
-// });
-$(document).ready(function() {
-  $('#submittedNumber').click(function() {
-    const inputtedNumber = parseInt($('#number').val());
-    $('#number').val("");
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
-    function checkNumber(number) {
-      if (isNaN(number) || number < 0) {
-        return new Error("Not a valid number!");
-      } else {
-        return true;
-      }
-    }
+$(document).ready(function(){
 
-    try {
-      const isNumberValid = checkNumber(inputtedNumber);
-      if (isNumberValid instanceof Error) {
-        console.error(isNumberValid.message);
-        throw RangeError("Not a valid number!");
-      } else {
-        console.log("Try was successful, so no need to catch!");
-        $('#displayNumber').text("This number is valid. You may continue.")
-      }
-    } catch(error) {
-      console.error(`Red alert! We have an error: ${error.message}`)
-    }
-  });
 });
